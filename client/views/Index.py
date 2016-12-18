4dc63ae65c3b812272c479bed2b00bcbf95bef20
c475 <danjamesbond@gmail.com>
2016-12-17 19:21:15 -0800

    .

--- client/views/Index.py
+++ client/views/Index.py
@@ -1,12 +1,10 @@
-from credentials import CROSSBAR_KEY
 from django.shortcuts import render
 from django.auth.decorators import login_required
-
-
-def generate_key()
 
 
 # Create your views here.
 @login_required
 def Index(request):
-	render(request, "index.html", context=)
+	render(request, "index.html", context={
+		'user': request.user
+	})
---
+++ scripts/install.sh
@@ -0,0 +1,12 @@
+apt-get update
+apt-get upgrade
+apt-get dist-upgrade
+apt-get install build-essential
+apt-get auto-remove
+
+sudo apt-get install nginx
+sudo apt-get install sqlite3
+
+sudo apt-get install python-dev
+sudo apt-get install python-pip
+sudo pip install -r /srv/requirements.txt
