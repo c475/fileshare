ca6871569ca531d49c518440adb1c8ba97867e8f
c475 <danjamesbond@gmail.com>
2016-12-19 20:22:45 -0800

    .

--- scripts/restart.py
+++ scripts/restart.py
@@ -1,9 +1,9 @@
 import subprocess
 
-subprocess.call(["sudo", "systemctl", "stop", "gunicorn"])
-subprocess.call(["sudo", "systemctl", "start", "gunicorn"])
+subprocess.call(["sudo", "systemctl", "stop", "gunicorn.service"])
+subprocess.call(["sudo", "systemctl", "start", "gunicorn.service"])
 
-subprocess.call(["sudo", "systemctl", "stop", "crossbar"])
-subprocess.call(["sudo", "systemctl", "start", "crossbar"])
+subprocess.call(["sudo", "systemctl", "stop", "websockets.service"])
+subprocess.call(["sudo", "systemctl", "start", "websockets.service"])
 
 subprocess.call(["sudo", "nginx", "-s", "reload"])
