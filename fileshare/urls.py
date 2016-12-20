2be49bd73b1e50d1d60de8460de479453c9fde0c
c475 <danjamesbond@gmail.com>
2016-12-19 20:28:25 -0800

    .

--- fileshare/urls.py
+++ fileshare/urls.py
@@ -1,10 +1,11 @@
 from django.conf.urls import url
+from django.contrib.auth.views import logout
 from client.views import *
 
 
 urlpatterns = [
     url(r'^index/', Index),
     url(r'^register/', Register.as_view()),
-    url(r'^logout/$', 'django.contrib.auth.views.logout', {'next_page': '/'}),
+    url(r'^logout/$', logout, {'next_page': '/'}),
     url(r'^login/$', Login.custom_login),
 ]
