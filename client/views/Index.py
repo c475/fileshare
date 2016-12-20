1bac61f0596e2af8a7b632a9cb0c2f235b8be082
c475 <danjamesbond@gmail.com>
2016-12-19 20:23:46 -0800

    .

--- client/views/Index.py
+++ client/views/Index.py
@@ -1,9 +1,8 @@
 from django.shortcuts import render
-from django.auth.decorators import login_required
+from django.contrib.auth.decorators import login_required
 
 
-# Create your views here.
-@login_required
+# @login_required
 def Index(request):
 	render(request, "index.html", context={
 		'user': request.user
